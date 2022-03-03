152ce4e5956e167ec1e410b6520de0c11c4e4cf8
Helder Sérvio <heldersrvio@gmail.com>
2022-03-02 22:25:09 -0300

    FEAT: REMOVE COMMENT FROM APP.JS

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ mongoose.connect(mongoDB);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
